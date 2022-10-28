6de0cca0b7971adc53245ae87c2b14138952324f
Richard S <bschwabe@pharmacy56>
2022-10-29 02:06:12 +0300

    added simple greeting cog

---
+++ cogs/greetings.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands 
+
+
+class Greetings(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        await message.add_reaction("✅")
+    
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member):
+        await ctx.send(f"Hello {member.name}")
+        
+async def setup(bot):
+    await bot.add_cog(Greetings(bot))
