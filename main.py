33c428c165e16ff207c3eee5ec5a0da98cfdfec2
Richard S <bschwabe@pharmacy56>
2022-10-28 03:23:17 +0300

    Settings updated with logging configuration

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import settings
 import discord 
 from discord.ext import commands
-
+    
+logger = settings.logging.getLogger("bot")
+    
 def run():
     intents = discord.Intents.default()
     
@@ -9,11 +11,10 @@ def run():
     
     @bot.event 
     async def on_ready():
-        print(bot.user)
-        print(bot.user.id)
-        print("________________")
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+        
     
-    bot.run(settings.DISCORD_API_SECRET)
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
     run()
--- settings.py
+++ settings.py
@@ -1,7 +1,56 @@
 import os
+import logging
+from logging.config import dictConfig
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
+
+
+LOGGING_CONFIG = {
+    "version": 1, 
+    "disabled_existing_loggers": False, 
+    "formatters":{
+        "verbose":{
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard":{
+            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
+        }
+    }, 
+    "handlers":{
+        "console": {
+            'level': "DEBUG", 
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        }, 
+        "console2": {
+            'level': "WARNING", 
+            'class': "logging.StreamHandler",
+            'formatter': "standard"
+        }, 
+        "file": {
+            'level': "INFO", 
+            'class': "logging.FileHandler",
+            'filename': "logs/infos.log",
+            'mode': "w", 
+            'formatter': "verbose"
+        }, 
+    }, 
+    "loggers":{
+        "bot": {
+            'handlers': ['console'],
+            "level": "INFO", 
+            "propagate": False
+        }, 
+        "discord": {
+            'handlers': ['console2', "file"],
+            "level": "INFO", 
+            "propagate": False
+        }
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
