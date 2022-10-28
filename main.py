72a381595ea867260a5b05f1c6a04dc02003dd75
Richard S <bschwabe@pharmacy56>
2022-10-28 03:39:19 +0300

    first command

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ logger = settings.logging.getLogger("bot")
     
 def run():
     intents = discord.Intents.default()
+    intents.message_content = True
     
     bot = commands.Bot(command_prefix="!", intents=intents)
     
     @bot.event 
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
-        
+    
+    @bot.command(
+        aliases=['p'],
+        help="This is help",
+        description="This is description",
+        brief = "This is brief",
+        enabled=True, 
+        hidden=True
+    )
+    async def ping(ctx):
+        """ Answers with pong """
+        await ctx.send("pong")
     
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
