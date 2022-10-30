1b1fde5245919f459464a03fdfb6b0a6b115fb14
Richard S <bschwabe@pharmacy56>
2022-10-30 03:55:50 +0200

    initial views

---
+++ initial_view.py
@@ -0,0 +1,53 @@
+import settings
+import discord 
+from discord.ext import commands
+    
+logger = settings.logging.getLogger("bot")
+    
+class SimpleView(discord.ui.View):
+    
+    def __init__(self):
+        super().__init__()
+        self.foo = None
+        
+    
+    @discord.ui.button(label="Say Hello", style=discord.ButtonStyle.green)
+    async def hello(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("World", ephemeral=True)
+        self.foo = True 
+        self.stop()
+        
+        
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def cancel(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.foo = False
+        self.stop()
+
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+    
+    bot = commands.Bot(command_prefix="!", intents=intents)
+    
+    @bot.event 
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.command()
+    async def button(ctx):
+        view = SimpleView()
+        await ctx.send(view=view)
+        await view.wait()
+        
+        if view.foo is None:
+            logger.error("Timedout")
+        elif view.foo:
+            logger.info("Confirmed / Finished")
+        else:
+            logger.info("Cancelling")
+    
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+if __name__ == "__main__":
+    run()
