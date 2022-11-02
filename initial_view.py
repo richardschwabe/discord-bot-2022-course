5645c5fd87880da7c7954bf3f75c7bdd5afa4f47
Richard S <bschwabe@pharmacy56>
2022-11-02 13:58:01 +0200

    added default for purge command

--- initial_view.py
+++ initial_view.py
@@ -7,21 +7,46 @@ logger = settings.logging.getLogger("bot")
     
 class SimpleView(discord.ui.View):
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, timeout):
+        super().__init__(timeout=timeout)
         self.foo = None
         
+    async def disable_all(self):
+        for item in self.children:
+            item.disabled = True 
+            
+        await self.message.edit(view=self)
+        
+    async def on_timeout(self) -> None:
+        await self.message.channel.send("Timed out!")
+        await self.disable_all()
     
-    @discord.ui.button(label="Say Hello", style=discord.ButtonStyle.green)
-    async def hello(self, interaction:discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("World", ephemeral=True)
+    @discord.ui.button(label="Say Hello", 
+                       style=discord.ButtonStyle.success)
+    async def hello(self, 
+                    interaction:discord.Interaction, 
+                    button: discord.ui.Button):
+        await interaction.response.send_message("World")
         self.foo = True 
         self.stop()
         
         
-    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-    async def cancel(self, interaction:discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("Cancelling", ephemeral=True)
+    @discord.ui.button(label="Cancel", 
+                       style=discord.ButtonStyle.danger)
+    async def cancel(self, 
+                     interaction:discord.Interaction, 
+                     button: discord.ui.Button):
+        await interaction.response.send_message("Cancelling")
+        self.foo = False
+        self.stop()
+        
+    @discord.ui.button(label="Disabled", 
+                       disabled=True,
+                       style=discord.ButtonStyle.gray)
+    async def disabled(self, 
+                       interaction:discord.Interaction, 
+                       button: discord.ui.Button):
+        await interaction.response.send_message("Cancelling")
         self.foo = False
         self.stop()
 
@@ -34,20 +59,32 @@ def run():
     @bot.event 
     async def on_ready():
         await utils.load_videocmds(bot)
-
+    
     @bot.command()
     async def button(ctx):
-        view = SimpleView()
-        await ctx.send(view=view)
-        await view.wait()
+        view = SimpleView(timeout=5)
         
+        button = discord.ui.Button(style=discord.ButtonStyle.primary, 
+                                label="Open URL", 
+                                url="https://www.youtube.com/@richardschwabe")
+        
+        view.add_item(button)
+        
+        message = await ctx.send(view=view)
+        view.message = message
+        await view.wait()
+                
         if view.foo is None:
             logger.error("Timedout")
         elif view.foo:
             logger.info("Confirmed / Finished")
         else:
             logger.info("Cancelling")
-    
+        
+        await view.disable_all()
+        
+        # await message.delete()
+        
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
--- videocmds/purge.py
+++ videocmds/purge.py
@@ -6,8 +6,10 @@ import settings
 logger = settings.logging.getLogger(__name__)
 
 @commands.command()
-async def purge(ctx, channel : discord.TextChannel, limit : int = 100):
+async def purge(ctx, channel : discord.TextChannel = None, limit : int = 100):
     """ Delete N number of messages in a channel """
+    if channel is None:
+        channel = ctx.message.channel
     await channel.purge(limit=limit)
 
 async def setup(bot):
