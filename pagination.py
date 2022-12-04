6e8753179bb5f23d3cf8751520b15639931a7fd7
Richard Schwabe <bschwabe@richardschwabe.de>
2022-12-04 18:41:18 +0200

    pagination example

--- pagination.py
+++ pagination.py
@@ -1,27 +1,113 @@
 
 import settings
-import discord 
+import discord
 from discord.ext import commands
 import utils
-    
+
 logger = settings.logging.getLogger("bot")
-        
+
+class PaginationView(discord.ui.View):
+    current_page : int = 1
+    sep : int = 5
+
+    async def send(self, ctx):
+        self.message = await ctx.send(view=self)
+        await self.update_message(self.data[:self.sep])
+
+    def create_embed(self, data):
+        embed = discord.Embed(title="Example")
+        for item in data:
+            embed.add_field(name=item, value=item, inline=False)
+        return embed
+
+    async def update_message(self,data):
+        self.update_buttons()
+        await self.message.edit(embed=self.create_embed(data), view=self)
+
+    def update_buttons(self):
+        if self.current_page == 1:
+            self.first_page_button.disabled = True
+            self.prev_button.disabled = True
+            self.first_page_button.style = discord.ButtonStyle.gray
+            self.prev_button.style = discord.ButtonStyle.gray
+        else:
+            self.first_page_button.disabled = False
+            self.prev_button.disabled = False
+            self.first_page_button.style = discord.ButtonStyle.green
+            self.prev_button.style = discord.ButtonStyle.primary
+
+        if self.current_page == int(len(self.data) / self.sep) + 1:
+            self.next_button.disabled = True
+            self.last_page_button.disabled = True
+            self.last_page_button.style = discord.ButtonStyle.gray
+            self.next_button.style = discord.ButtonStyle.gray
+        else:
+            self.next_button.disabled = False
+            self.last_page_button.disabled = False
+            self.last_page_button.style = discord.ButtonStyle.green
+            self.next_button.style = discord.ButtonStyle.primary
+
+    def get_current_page_data(self):
+        until_item = self.current_page * self.sep
+        from_item = until_item - self.sep
+        if self.current_page == 1:
+            from_item = 0
+            until_item = self.sep
+        if self.current_page == int(len(self.data) / self.sep) + 1:
+            from_item = self.current_page * self.sep - self.sep
+            until_item = len(self.data)
+        return self.data[from_item:until_item]
+
+
+    @discord.ui.button(label="|<",
+                       style=discord.ButtonStyle.green)
+    async def first_page_button(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.current_page = 1
+
+        await self.update_message(self.get_current_page_data())
+
+    @discord.ui.button(label="<",
+                       style=discord.ButtonStyle.primary)
+    async def prev_button(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.current_page -= 1
+        await self.update_message(self.get_current_page_data())
+
+    @discord.ui.button(label=">",
+                       style=discord.ButtonStyle.primary)
+    async def next_button(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.current_page += 1
+        await self.update_message(self.get_current_page_data())
+
+    @discord.ui.button(label=">|",
+                       style=discord.ButtonStyle.green)
+    async def last_page_button(self, interaction:discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer()
+        self.current_page = int(len(self.data) / self.sep) + 1
+        await self.update_message(self.get_current_page_data())
+
+
 def run():
     intents = discord.Intents.all()
-    
+
     bot = commands.Bot(command_prefix="!", intents=intents)
-    
-    @bot.event 
+
+    @bot.event
     async def on_ready():
         await utils.load_videocmds(bot)
-    
+
     @bot.command()
     async def paginate(ctx):
-        embed = discord.Embed()
-        await ctx.send(embed=embed)
-        
-        
+        data = range(1, 15)
+        pagination_view = PaginationView(timeout=None)
+        pagination_view.data = data
+        await pagination_view.send(ctx)
+
+
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
 
 if __name__ == "__main__":
     run()
