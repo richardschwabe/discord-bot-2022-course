254dde36a08a99dd0044cc8ce1da1d8905ad1d69
Richard Schwabe <bschwabe@richardschwabe.de>
2022-12-04 18:50:50 +0200

    added give roles example

---
+++ give_roles.py
@@ -0,0 +1,46 @@
+import random
+import settings
+import discord
+from discord.ext import commands
+
+logger = settings.logging.getLogger("bot")
+
+def run():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    async def setup_roles():
+        role_names = ['girl', 'boy', 'new', 'oldtime']
+        for role_item in role_names:
+            await bot.guilds[0].create_role(
+                name=role_item,
+                hoist=True
+            )
+
+    @bot.event
+    async def on_ready():
+        logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+        await setup_roles()
+
+
+    @bot.command()
+    async def add(ctx, member: discord.Member, role: discord.Role ):
+        if member and role:
+            await member.add_roles(role)
+        else:
+            await ctx.send("Not found...")
+
+    @bot.command()
+    async def remove(ctx, member: discord.Member, role: discord.Role ):
+        if member and role:
+            await member.remove_roles(role)
+        else:
+            await ctx.send("Not found...")
+
+
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
+if __name__ == "__main__":
+    run()
