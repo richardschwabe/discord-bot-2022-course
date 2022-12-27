d11e7991cbf5c511f05c18ac53ba2532bcb90712
Richard Schwabe <bschwabe@richardschwabe.de>
2022-12-27 10:00:59 +0200

    fixed temp voice after.channel check

--- cog_simple.py
+++ cog_simple.py
@@ -1,23 +1,22 @@
-import discord 
+import discord
 from discord.ext import commands
 import settings
-    
+
 logger = settings.logging.getLogger("bot")
-    
+
 def run():
     intents = discord.Intents.default()
     intents.message_content = True
-    
+
     bot = commands.Bot(command_prefix="!", intents=intents)
-    
-    @bot.event 
+
+    @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
 
         await bot.load_extension("cogs.greetings")
 
 
-        
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
 if __name__ == "__main__":
--- cogs/temporaryvoice.py
+++ cogs/temporaryvoice.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands 
+from discord.ext import commands
 import settings
 
 logger = settings.logging.getLogger(__name__)
@@ -7,28 +7,29 @@ logger = settings.logging.getLogger(__name__)
 # Intents.voice_states is required!
 
 class TemporaryVoice(commands.Cog):
-    
+
     temporary_channels = []
     temporary_categories = []
-    
+
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState)    :
         possible_channel_name = f"{member.nick}'s area"
-        if after.channel.name == "temp":
-           temp_channel = await after.channel.clone(name=possible_channel_name)
-           await member.move_to(temp_channel)
-           self.temporary_channels.append(temp_channel.id)
-        if after.channel.name == 'teams':
-            temporary_category = await after.channel.guild.create_category(name=possible_channel_name)
-            await temporary_category.create_text_channel(name="text")
-            temp_channel = await temporary_category.create_voice_channel(name="voice")
-            await member.move_to(temp_channel)
-            self.temporary_categories.append(temp_channel.id)
-            
-           
+        if after.channel:
+            if after.channel.name == "temp":
+                temp_channel = await after.channel.clone(name=possible_channel_name)
+                await member.move_to(temp_channel)
+                self.temporary_channels.append(temp_channel.id)
+            if after.channel.name == 'teams':
+                temporary_category = await after.channel.guild.create_category(name=possible_channel_name)
+                await temporary_category.create_text_channel(name="text")
+                temp_channel = await temporary_category.create_voice_channel(name="voice")
+                await member.move_to(temp_channel)
+                self.temporary_categories.append(temp_channel.id)
+
+
         if before.channel:
             if before.channel.id in self.temporary_channels:
                 if len(before.channel.members) == 0:
@@ -38,6 +39,6 @@ class TemporaryVoice(commands.Cog):
                     for channel in before.channel.category.channels:
                         await channel.delete()
                     await before.channel.category.delete()
-       
+
 async def setup(bot):
     await bot.add_cog(TemporaryVoice(bot))
