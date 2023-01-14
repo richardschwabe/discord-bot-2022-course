999cd1363826f8a3fdaf077d374d58484485d47c
Richard Schwabe <bschwabe@richardschwabe.de>
2023-01-14 19:32:49 +0200

    fixed wrong import at top, removed nsfw check

--- client_application_command.py
+++ client_application_command.py
@@ -1,17 +1,16 @@
-from re import A
 import settings
 import discord
 from discord.ext import commands
-from discord import app_commands
 
 logger = settings.logging.getLogger("bot")
 
 foo = "bar"
 
+
 def run():
     intents = discord.Intents.all()
 
-    bot = commands.Bot(command_prefix="!",intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.event
     async def on_ready():
@@ -20,11 +19,14 @@ def run():
         bot.tree.copy_global_to(guild=settings.GUILDS_ID)
         await bot.tree.sync(guild=settings.GUILDS_ID)
 
-    @bot.tree.command(description="Welcomes user", name="greetings", nsfw=True)
+    @bot.tree.command(description="Welcomes user", name="greetings")
     async def ciao(interaction: discord.Interaction):
-        await interaction.response.send_message(f"Ciao! {interaction.user.mention}", ephemeral=True)
+        await interaction.response.send_message(
+            f"Ciao! {interaction.user.mention}", ephemeral=True
+        )
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
+
 
 if __name__ == "__main__":
     run()
